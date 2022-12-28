7f9e0bde240a30921c2666e2857a012103876caa
MauroDoneda <mauro.doneda@gmail.com>
2022-12-28 12:08:29 +0000

    Add styles

--- monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import './card-list-styles.css'
 
 export class CardList extends Component {  
   render() {
     return (
       <div className='card-list'>
         {this.props.monsters.map(monster => 
-          <div key={monster.id}>
+          <div key={monster.id} className ='card-container'>
+            <img 
+              alt={`monster ${monster.name}`} 
+              src={`https://robohash.org/${monster.id}?set=2&size=180x180`} 
+            />
             <h1>{monster.name}</h1>
+            <h3>{monster.email}</h3>
           </div>
         )}
       </div>
