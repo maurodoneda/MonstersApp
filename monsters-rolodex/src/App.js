024da7cee6e549a16e230becc87bb3618c52f307
MauroDoneda <mauro.doneda@gmail.com>
2022-12-29 10:30:03 +0000

    Add useEffect to filter function

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,6 +7,7 @@ import SearchBox from './components/search-box/search-box.component';
 const App = () => {
   const [searchString, setSearchString] = useState('');
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([monsters]);
 
   const onSearchChange = (event) => {
     setSearchString(event.target.value.toLowerCase());
@@ -16,18 +17,22 @@ const App = () => {
       fetch("https://jsonplaceholder.typicode.com/users")
         .then(res => res.json())
         .then(users => setMonsters(users))
-  },    [monsters]);
+  },    []);
 
+  useEffect(()=>{
+    const filtered = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchString)
+    })
 
-  const filtered = monsters.filter(monster => {
-    return monster.name.toLowerCase().includes(searchString)
-  })
+    setFilteredMonsters(filtered);
+  }, [monsters, searchString])
+
 
   return (
     <div className="App">
       <h1 className='app-title'>Robot Search</h1>
       <SearchBox className='search-box' onChangeHandler={onSearchChange}/>
-      <CardList monsters={filtered}/>
+      <CardList monsters={filteredMonsters}/>
     </div>
   );
 }
